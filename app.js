32c6a6ef0c1bb32b30473ab503a719564eab4942
masud rana <masudranaacb@gmail.com>
2022-03-09 08:48:27 -0800

    ternary operator

--- app.js
+++ app.js
@@ -3,8 +3,15 @@
 // '',0,false,null,undefindd
 const number=-55;
 if(number){
-    console.log('truthy')
+    // console.log('truthy')
 }else{
-    console.log('falsy')
+    // console.log('falsy')
 }
+
+
 //  Ternary operator
+const money=200
+let foods;
+const food=money>100? 'Muton':'Birani'
+console.log(food)
+
